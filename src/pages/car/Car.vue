9ab62afde014154a28ef7aadc44cbb259e1c535a
cyk970510 <1050092854@qq.com>
2019-04-15 22:44:22 +0800

    car-maybeLike

--- src/pages/car/Car.vue
+++ src/pages/car/Car.vue
@@ -2,7 +2,18 @@
   <div class="car">
     <car-header :headerList="headerList"></car-header>
     <car-goods @tocommit="getcommit" :commitinfo="commitinfo"></car-goods>
+    <car-maybe :maybeList="maybeList"></car-maybe>
     <common-footer></common-footer>
+    <!--弹窗-->
+    <div class="commitToDelect" v-if="isshow">
+      <div class="content">
+        <div class="title border">是否确定删除该商品</div>
+        <div class="bottom">
+          <span class="cancle" @click="cancle">取消</span>
+          <span class="commit" @click="yescommit">确定</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,9 +22,11 @@ import axios from 'axios'
 import CarHeader from './components/Header'
 import CarGoods from './components/Goods'
 import CommonFooter from '../../common/footer/footer'
+import CarMaybe from './components/MaybeLike'
 export default {
   name: 'car',
   components: {
+    CarMaybe,
     CommonFooter,
     CarGoods,
     CarHeader
@@ -21,8 +34,8 @@ export default {
   data () {
     return {
       headerList: [],
-      // maybeList: [],
-      // isshow: false,
+      maybeList: [],
+      isshow: false,
       commitinfo: 0
     }
   },
@@ -39,37 +52,74 @@ export default {
         this.headerList = data.headerList
       }
     },
-    // // 获取内容信息
-    // getInfo () {
-    //   axios.get('/api/car.json')
-    //     .then(this.getInfoSucc)
-    // },
-    // getInfoSucc (res) {
-    //   res = res.data
-    //   if (res.ret && res.data) {
-    //     const data = res.data
-    //     this.maybeList = data.maybeList
-    //   }
-    // },
+    // 获取内容信息
+    getInfo () {
+      axios.get('/api/car.json')
+        .then(this.getInfoSucc)
+    },
+    getInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.maybeList = data.maybeList
+      }
+    },
     getcommit () {
       this.isshow = !this.isshow
+    },
+    cancle () {
+      this.isshow = !this.isshow
+      this.commitinfo = 0
+    },
+    yescommit () {
+      this.isshow = !this.isshow
+      this.commitinfo += 1
     }
-    // cancle () {
-    //   this.isshow = !this.isshow
-    //   this.commitinfo = 0
-    // },
-    // yescommit () {
-    //   this.isshow = !this.isshow
-    //   this.commitinfo += 1
-    // }
   },
   mounted () {
     this.getHeaderInfo()
-    // this.getInfo()
+    this.getInfo()
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .car
+    margin 0
+    padding 0
+    overflow scroll
+    .commitToDelect
+      z-index 900
+      position absolute
+      top 0
+      left 0
+      width 100%
+      height 100%
+      background: rgba(0,0,0,0.5)
+      .content
+        position relative
+        width 70%
+        height 3.5rem
+        margin 0 auto
+        margin-top 4rem
+        border-radius .2rem
+        background white
+        .title
+          position relative
+          width 100%
+          padding 1.2rem 0
+          border-radius .2rem .2rem 0 0
+          text-align center
+          font-size .3rem
+        .bottom
+          position relative
+          width 100%
+          padding .26rem 0
+          text-align center
+          .cancle
+            padding .26rem 19.5%
+            border-radius 0 0 0 .2rem
+          .commit
+            padding .26rem 19.5%
+            border-radius 0 0 .2rem 0
 </style>
