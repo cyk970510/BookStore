77047cbe32e5a1b4d0f71b68314f5b405ee6bbd8
cyk970510 <1050092854@qq.com>
2019-04-15 22:34:07 +0800

    car-header

--- src/pages/car/Car.vue
+++ src/pages/car/Car.vue
@@ -1,10 +1,66 @@
 <template>
-  <div class="car"></div>
+  <div class="car">
+    <car-header :headerList="headerList"></car-header>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+import CarHeader from './components/Header'
 export default {
-  name: 'car'
+  name: 'car',
+  components: {
+    CarHeader
+  },
+  data () {
+    return {
+      headerList: []
+      // maybeList: [],
+      // isshow: false,
+      // commitinfo: 0
+    }
+  },
+  methods: {
+    // 获取头部信息
+    getHeaderInfo () {
+      axios.get('/api/publicheader.json')
+        .then(this.getHeaderInfoSucc)
+    },
+    getHeaderInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.headerList = data.headerList
+      }
+    }
+    // // 获取内容信息
+    // getInfo () {
+    //   axios.get('/api/car.json')
+    //     .then(this.getInfoSucc)
+    // },
+    // getInfoSucc (res) {
+    //   res = res.data
+    //   if (res.ret && res.data) {
+    //     const data = res.data
+    //     this.maybeList = data.maybeList
+    //   }
+    // },
+    // getcommit () {
+    //   this.isshow = !this.isshow
+    // },
+    // cancle () {
+    //   this.isshow = !this.isshow
+    //   this.commitinfo = 0
+    // },
+    // yescommit () {
+    //   this.isshow = !this.isshow
+    //   this.commitinfo += 1
+    // }
+  },
+  mounted () {
+    this.getHeaderInfo()
+    // this.getInfo()
+  }
 }
 </script>
 
